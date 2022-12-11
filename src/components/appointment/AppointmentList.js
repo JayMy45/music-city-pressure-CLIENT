6b250120e39b604714bde312396c455cea27066d
Jeremy Myers <echelonical@gmail.com>
2022-12-11 01:41:03 -0600

    styling updates

--- src/components/appointment/AppointmentList.js
+++ src/components/appointment/AppointmentList.js
@@ -46,47 +46,42 @@ export const AppointmentList = () => {
                     {
                         appointments.map(appointment => {
                             return <React.Fragment key={`appointment--${appointment.id}`}>
-                                <div className="appointment__request is-4-tablet is-3-desktop mb-6">
-                                    <div className="card">
+                                <div className="appointment__request is-4-tablet is-3-desktop mb-6 column">
+                                    <div className="card ">
 
                                         {
                                             customers.map(customer => {
                                                 return <div className="card-content" key={`customer--${customer.id}`}>
-                                                    <div className="card-image has-text-centered px-7 mb-3">
-                                                        <figure className="image is-5by4">
+                                                    <div className="card-image has-text-centered px-0">
+                                                        <figure className="image is-4by3">
                                                             <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCDeAQKGIjC916XeJAnv7JuDFj6GHoduUGKAZoFVVWJ4IkzHj0nRNvcdt_PjZ1tReaksMyOORmIwZwA_hBJr72xq9QP3Je&usqp=CAE" alt="" />
                                                         </figure>
                                                     </div>
-                                                    <div className="media">
-                                                        <div className="media-content mr-3 mt-5" >
+                                                    <div className="ml-5 mt-5 is-centered">
+                                                        <button className="button" onClick={() => navigate(`/appointments/update/${appointment.id}`)}>
+                                                            <span className="icon">
+                                                                <ion-icon name="repeat-outline"></ion-icon>
+                                                            </span>
+                                                            <span className="is-uppercase is-small">Update</span>
+                                                        </button>
+                                                        <button className="button" onClick={(evt) => confirmDelete(evt, appointment)}>
+                                                            <span className="icon">
+                                                                <ion-icon name="trash-outline"></ion-icon>
+                                                            </span>
+                                                            <span className="is-uppercase is-small">Delete</span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="">
+                                                        <div className="mt-5" >
                                                             <p className=""><strong>Service:</strong> <Link to={`/appointments/${appointment.id}`}>{appointment.service_type.name}</Link></p>
+                                                            <p><strong>Details:</strong></p>
                                                             <div className="paragraph">
-                                                                <p><strong>Details:</strong> {appointment.request_details}</p>
+                                                                <p>{appointment.request_details}</p>
                                                             </div>
                                                             <p><strong>Address:</strong> {customer.address}</p>
                                                             <footer className="mb-3 mt-2"><em>Request Date:</em> {appointment.request_date}</footer>
                                                         </div>
-                                                        <div className="media-left ml-3 mt-5">
-                                                            <div>
-                                                                <button className="button  mb-1" onClick={() => navigate(`/appointments/update/${appointment.id}`)}>
-                                                                    <span className="icon">
-                                                                        <ion-icon name="repeat-outline"></ion-icon>
-                                                                    </span>
-                                                                    <span>Update</span>
-                                                                </button>
-                                                            </div>
-                                                            <div className="appt__progress">
-                                                                <h4>Progress</h4>
-                                                            </div>
 
-                                                            <div>
-                                                                <button className="button is-inverted is-black is-large ml-5" onClick={(evt) => confirmDelete(evt, appointment)}>
-                                                                    <span className="icon">
-                                                                        <ion-icon name="trash-outline"></ion-icon>
-                                                                    </span>
-                                                                </button>
-                                                            </div>
-                                                        </div>
                                                     </div>
                                                 </div>
                                             })
