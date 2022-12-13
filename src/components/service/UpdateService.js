a94bb713154fc1d40947b26660ee53dd4f9c7341
Jeremy Myers <echelonical@gmail.com>
2022-12-13 15:30:56 -0600

    update to service form

--- src/components/service/UpdateService.js
+++ src/components/service/UpdateService.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import { getServiceById, saveEditedService } from "../../managers/ServiceManager"
+import { getEquipmentById, getEquipments } from "../../managers/EquipmentManager"
+import { getServiceById, getServices, saveEditedService } from "../../managers/ServiceManager"
 
 export const UpdateService = () => {
 
     const navigate = useNavigate()
     const { serviceId } = useParams()
-
+    const [equipments, setEquipments] = useState([])
     const [currentService, setCurrentService] = useState({
         name: "",
         description: "",
         details: "",
         price: 0,
-        equipment_id: 1
+        equipment_id: 0
     })
 
+
     useEffect(() => {
-        getServiceById(serviceId)
-            .then(setCurrentService)
-    })
+        getEquipments()
+            .then(setEquipments)
+    }, [])
+
 
     useEffect(() => {
         getServiceById(serviceId)
@@ -37,42 +40,63 @@ export const UpdateService = () => {
     return <>
         <form>
             <h2>Hellow UPDATE Worldie</h2>
-            {/* <div>
+            <div>
                 <div>
-                    <label>Update Service Name: <span><em>Current Service Selection</em> {currentAppt?.service_type?.name}</span></label>
+                    <label> <span><em>current service selection</em> {currentService.name}</span></label>
                 </div>
-                <select name="service_type_id" className="drop__down" onChange={changeAppointmentState} value={currentAppt.service_type_id}>
+
+            </div>
+            {/* <div>
+                <label>Update Request Date</label>
+                <input type="date" name="request_date" required autoFocus className=""
+                    value={currentService.request_date}
+                    onChange={changeServiceState} />
+            </div> */}
+
+            {/* <div>
+                <label>consultation</label>
+                <input type="checkbox" />
+            </div> */}
+
+
+            <div>
+                <label>Description</label>
+                <input type="text" name="description" required autoFocus className=""
+                    value={currentService.description}
+                    placeholder={currentService.description}
+                    onChange={changeServiceState} />
+            </div>
+
+            <div>
+                <label>Request Details</label>
+                <input type="text" name="details" required autoFocus className=""
+                    value={currentService.details}
+                    placeholder={currentService.details}
+                    onChange={changeServiceState} />
+            </div>
+            <div>
+                <label>Price</label>
+                <input type="text" name="price" required autoFocus className=""
+                    value={currentService.price}
+                    placeholder={currentService.price}
+                    onChange={changeServiceState} />
+            </div>
+
+            <div>
+                <div>
+                    <label> <span><em>current service selection</em> {currentService.equipment_id}</span></label>
+                </div>C
+                <select name="equipment_id" className="drop__down" onChange={changeServiceState} value={currentService.id}>
                     <option value={0}>Change Service Type</option>
                     {
-                        services.map(service => {
-                            return <option value={`${service.id}`} key={`service--${service.id}`}>{service.name}</option>
+                        equipments.map(equipment => {
+                            return <option value={`${equipment.id}`} key={`equipment--${equipment.id}`}>{equipment.label}</option>
                         })
                     }
                 </select>
             </div>
-            <div>
-                <label>Update Request Date</label>
-                <input type="date" name="request_date" required autoFocus className=""
-                    value={currentAppt.request_date}
-                    onChange={changeAppointmentState} />
-            </div>
 
             <div>
-                <label>consultation</label>
-                <input type="checkbox" />
-            </div>
-
-
-            <div>
-                <label>Request Details</label>
-                <input type="text" name="request_details" required autoFocus className=""
-                    value={currentAppt.request_details}
-                    placeholder={currentAppt.request_details}
-                    onChange={changeAppointmentState} />
-            </div> */}
-
-
-            {/* <div>
                 <button type="submit" onClick={evt => {
                     // Prevent form from being submitted
                     evt.preventDefault()
@@ -91,7 +115,7 @@ export const UpdateService = () => {
                         .then(() => navigate("/services"))
                 }}
                     className="">Update Service</button>
-            </div> */}
+            </div>
 
         </form>
     </>
