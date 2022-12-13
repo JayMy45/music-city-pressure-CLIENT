ed1895ac585fbde2a9d885f95dba90cf6991fe1f
Jeremy Myers <echelonical@gmail.com>
2022-12-12 23:31:24 -0600

    update service implementation

---
+++ src/components/service/UpdateService.js
@@ -0,0 +1,9 @@
+import { useSearchParams } from "react-router-dom"
+
+export const UpdateService = () => {
+    const { serviceId } = useSearchParams()
+    const [services, setServices] = useState()
+
+
+    return <></>
+}
