69ef40bcb6109001c85c37b3d27d092e7a060a2d
Jeremy Myers <echelonical@gmail.com>
2023-01-04 22:44:46 -0600

    navbar hamburger operational

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,15 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 // import Logo from "/Users/jeremymyers/workspace/level-up-CLIENT/level-up-react-CLIENT/src/logo192.png"
 import "./NavBar.css"
 
 export const NavBar = () => {
     const navigate = useNavigate()
+    const [isActive, setIsActive] = useState(false)
 
+    const closeHamburger = () => {
+        setIsActive(!isActive)
+    }
 
 
     return (
-        <nav className="navbar has-shadow is-warning mb-5" role="navigation" aria-label="main navigation">
+        <nav className="navbar has-shadow is-warning mb-5 is-fixed-top" role="navigation" aria-label="main navigation">
 
             <div className="navbar-brand">
                 <a className="navbar-item" href="/">
@@ -18,79 +22,80 @@ export const NavBar = () => {
 
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 {/* <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" id="burger" onClick={toggleMenu}> */}
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" id="burger" onClick={() => {
-                    const burger = document.getElementById('burger');
-                    const navLinks = document.getElementById('navbarBasicExample');
-                    burger.classList.toggle('is-active');
-                    navLinks.classList.toggle('is-active');
-                }}>
+                <a
+                    onClick={() => { setIsActive(!isActive) }}
+                    role="button"
+                    className={`navbar-burger burger ${isActive ? 'is-active' : ""}`}
+                    aria-label="menu navigation"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                >
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
-
-                {/* <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a> */}
             </div>
 
-            <div className="navbar-end nav-links">
-                <div className="navbar-menu is">
-                    {
-                        (localStorage.getItem("mc_token") !== null) ?
-                            <div>
-                                <ul className="navbar-item">
-                                    <div className="navbar-item">
-                                        <li className="navbar__item">
-                                            <Link to="/appointments/create">Make an Appointment</Link>
-                                        </li>
-                                    </div>
-                                    <div className="navbar-item">
-                                        <li className="navbar__item">
-                                            <Link to="/appointments">Appointments</Link>
-                                        </li>
-                                    </div>
-                                    <div className="navbar-item">
-                                        <li className="navbar__item">
-                                            <Link to="/services">Services</Link>
-                                        </li>
-                                    </div>
-                                </ul>
-                            </div>
-
-                            : <></>
-                    }
-                    {
-
-                        (localStorage.getItem("mc_token") !== null) ?
-                            <li className="navbar-item">
-                                <button className="nav-link fakeLink is-link"
-                                    onClick={() => {
-                                        localStorage.removeItem("mc_token");
-                                        localStorage.removeItem("is_staff");
-                                        localStorage.removeItem("is_superuser")
-                                        navigate('/login')
-                                    }}
-                                >Logout</button>
-                            </li> :
-                            <>
+            <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ""}`} >
+                <div className="navbar-end ">
+                    <div className="">
+                        {
+                            (localStorage.getItem("mc_token") !== null) ?
                                 <div>
-                                    <div className="navbar-item">
+                                    <ul className="navbar-item">
                                         <div className="navbar-item">
-                                            <ul className="nav-item">
-                                                <Link className="nav-link is-link" to="/login">Login</Link>
-                                            </ul>
+                                            <li className="navbar__item">
+                                                <Link to="/appointments/create" onClick={closeHamburger}>Make an Appointment</Link>
+                                            </li>
                                         </div>
                                         <div className="navbar-item">
-                                            <ul className="nav-item">
-                                                <Link className="nav-link is-link" to="/register">Register</Link>
-                                            </ul>
+                                            <li className="navbar__item">
+                                                <Link to="/appointments" onClick={closeHamburger}>Appointments</Link>
+                                            </li>
+                                        </div>
+                                        <div className="navbar-item">
+                                            <li className="navbar__item">
+                                                <Link to="/services" onClick={closeHamburger}>Services</Link>
+                                            </li>
+                                        </div>
+                                    </ul>
+                                </div>
+
+                                : <></>
+                        }
+                    </div>
+                    <div className="is-flex mr-4 is-vcentered">
+                        {
+                            (localStorage.getItem("mc_token") !== null) ?
+                                <li className="navbar-item">
+                                    <button className="nav-link fakeLink is-link"
+                                        onClick={() => {
+                                            closeHamburger();
+                                            localStorage.removeItem("mc_token");
+                                            localStorage.removeItem("is_staff");
+                                            localStorage.removeItem("is_superuser")
+                                            navigate('/login')
+                                        }}
+                                    >Logout</button>
+                                </li> :
+                                <>
+                                    <div>
+                                        <div className="navbar-item">
+                                            <div className="navbar-item">
+                                                <ul className="nav-item">
+                                                    <Link className="nav-link is-link" to="/login" onClick={closeHamburger}>Login</Link>
+                                                </ul>
+                                            </div>
+                                            <div className="navbar-item">
+                                                <ul className="nav-item">
+                                                    <Link className="nav-link is-link" to="/register" onClick={closeHamburger}>Register</Link>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </>
-                    }
+                                </>
+                        }
+                    </div>
                 </div>
             </div>
         </nav >
