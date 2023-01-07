65c4965228f19f3d6dd24758d68c12fe3f7c4c9d
Jeremy Myers <echelonical@gmail.com>
2023-01-07 14:21:40 -0600

    UpdateService module revised to handle labels, image, and tool updates properly

--- src/components/service/UpdateService.js
+++ src/components/service/UpdateService.js
@@ -8,6 +8,7 @@ export const UpdateService = () => {
     const navigate = useNavigate()
     const { serviceId } = useParams()
     const [equipments, setEquipments] = useState([])
+    const [checkedOptions, setCheckedOptions] = useState(new Set())
     const [currentService, setCurrentService] = useState({
         name: "",
         description: "",
@@ -35,6 +36,23 @@ export const UpdateService = () => {
         })
     }
 
+    const showWidget = (clickEvent) => {
+        clickEvent.preventDefault()
+        let widget = window.cloudinary.createUploadWidget({
+            cloudName: `dp04hh5pz`,
+            uploadPreset: `gv9plrcj`
+        },
+            (error, result) => {
+                if (!error && result && result.event === "success") {
+                    console.log(result.info.url)
+                    const copy = structuredClone(currentService)
+                    copy.image = result.info.url
+                    setCurrentService(copy)
+                }
+            });
+        widget.open()
+    }
+
     return <>
         <form>
             <h2>Hellow UPDATE Worldie</h2>
@@ -50,6 +68,29 @@ export const UpdateService = () => {
                     placeholder={currentService.description}
                     onChange={changeServiceState} />
             </div>
+            <div>
+                <div>
+                    <label>Label</label>
+                </div>
+                <input type="text" name="label" required autoFocus className=""
+                    value={currentService.label}
+                    onChange={changeServiceState} />
+            </div>
+            <div>
+                <div>
+                    {
+                        currentService.image !== ""
+                            ? <figure className="service__image is-size-4"><img src={currentService.image} alt="preview" /></figure>
+                            : <></>
+                    }
+                </div>
+                <button
+                    onClick={(clickEvent) => showWidget(clickEvent)}
+                    className="btn btn-primary">
+                    Update Image
+                </button>
+            </div>
+
 
             <div>
                 <label>Request Details</label>
@@ -79,6 +120,23 @@ export const UpdateService = () => {
                     }
                 </select>
             </div>
+            <div>
+
+                {equipments.map(equip => (<div className="ml-2 mr-2" key={`equipment--${equip.id}`}>
+
+                    <input className="mr-2" value={equip.id}
+                        onChange={(e) => {
+                            const copy = new Set(checkedOptions)
+                            if (copy.has(equip.id)) {
+                                copy.delete(equip.id)
+                            } else { copy.add(equip.id) }
+                            setCheckedOptions(copy)
+                        }
+                        } type="checkbox" />
+                    {equip.label}
+                </div>))
+                }
+            </div>
 
             <div>
                 <button type="submit" onClick={evt => {
@@ -86,12 +144,15 @@ export const UpdateService = () => {
                     evt.preventDefault()
 
                     const service = {
+
                         id: currentService.id,
                         name: currentService.name,
+                        label: currentService.label,
+                        image: currentService.image,
                         description: currentService.description,
                         details: currentService.details,
-                        price: currentService.price,
-                        equipment_id: currentService.equipment_id
+                        tools: Array.from(checkedOptions),
+                        price: parseFloat(currentService.price)
                     }
 
                     // Send POST request to your API
