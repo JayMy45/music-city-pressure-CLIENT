a0f0372abe70f6724a4f27f7df74825d0abc2c46
Jeremy Myers <echelonical@gmail.com>
2023-02-01 17:25:50 -0600

    alphabetize specialties by label

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -70,13 +70,25 @@ export const Employee = ({ emp, superUser, mCPressure, fetchEmployees }) => {
                     <div className="employee__specialty--delete-btn column">
                         <div>
                             <h2 className="center is-size-7"><strong><u>Specialties</u></strong></h2>
+                            {emp.specialty.sort((a, b) => {
+                                if (a.label < b.label) return -1;
+                                if (a.label > b.label) return 1;
+                                return 0;
+                            }).map(special => {
+                                return <div key={`specialty--${special.id}`}>
+                                    <p className="center is-size-7">{special.label}</p>
+                                </div>
+                            })
+                            }
+
+                            {/* Old way to get specialties
                             {
                                 emp.specialty.map(special => {
                                     return <div key={`specialty--${special.id}`}>
                                         <p className="center is-size-7">{special.label}</p>
                                     </div>
                                 })
-                            }
+                            } */}
                         </div>
                         <div className="center is-flex">
                             {
