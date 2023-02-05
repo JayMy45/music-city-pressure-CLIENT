77270fd5625fba46d79b01331ccbf999473fd9b1
Jeremy Myers <echelonical@gmail.com>
2023-02-05 16:35:37 -0600

    customerUpdate module functional can be routed to and shows minimal information

--- src/components/customer/CustomerUpdate.js
+++ src/components/customer/CustomerUpdate.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { getCustomerById } from "../../managers/CustomerManager"
 
 export const CustomerUpdate = () => {
@@ -12,6 +12,9 @@ export const CustomerUpdate = () => {
         phone_number: "",
         location: []
     })
+
+    const navigate = useNavigate()
+
 
     const renderCustomer = useCallback(() => {
         if (customerId) {
@@ -26,8 +29,24 @@ export const CustomerUpdate = () => {
     }, [renderCustomer])
 
     return <>
-        Hellow Customer Update Worldie
-        {console.log(`${customer.full_name}`)}
-        <h2>{customer.full_name}</h2>
+        <div className="mt-5" id="navbar__space">
+            <h1 className=" ml-5 mt-5">Update Customer</h1>
+        </div>
+        <div>
+            <div className="mt-5 mb-5 ml-5 ">
+                <div>
+                    <button className="btn__customer--update__1 button is-small has-background-light has-text-link-dark ml-5" onClick={() => navigate(`/customers`)}>Back to Customers</button>
+                </div>
+                <div>
+                    <button className="btn__customer--update__1 button is-small has-background-light has-text-link-dark ml-5 mt-1" onClick={() => navigate(`/appointments`)}>Back to Appointments</button>
+                </div>
+            </div>
+        </div>
+
+        <form>
+            Hellow Customer Update Worldie
+            {console.log(`${customer.full_name}`)}
+            <h2>{customer.full_name}</h2>
+        </form>
     </>
 }
